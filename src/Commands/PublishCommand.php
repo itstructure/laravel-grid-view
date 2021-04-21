3f4e72868e086ace62f6823520e53738059afb21
Andrey Girnik <girnikandrey@gmail.com>
2021-04-21 23:04:39 +0700

    Create PublishCommand

---
+++ src/Commands/PublishCommand.php
@@ -0,0 +1,70 @@
+<?php
+
+namespace Itstructure\GridView\Commands;
+
+use Illuminate\Console\Command;
+use Itstructure\GridView\GridViewServiceProvider;
+
+/**
+ * Class PublishCommand
+ *
+ * @package Itstructure\GridView\Commands
+ *
+ * @author Andrey Girnik <girnikandrey@gmail.com>
+ */
+class PublishCommand extends Command
+{
+    /**
+     * The name and signature of the console command.
+     * @var string
+     */
+    protected $signature = 'grid_view:publish '.
+    '{--force : Overwrite existing files by default. This option can not be used.}'.
+    '{--only= : Publish only specific part. Available parts: views, lang. This option can not be used.}';
+
+    /**
+     * The console command description.
+     * @var string
+     */
+    protected $description = 'Publish GridView package parts.';
+
+    /**
+     * Execute the console command.
+     * @return void
+     */
+    public function handle()
+    {
+        $this->info('Starting publication process of GridView package parts...');
+
+        $callArguments = ['--provider' => GridViewServiceProvider::class];
+
+        if ($this->option('only')) {
+            switch ($this->option('only')) {
+                case 'views':
+                    $this->info('Publish just a part: views.');
+                    $callArguments['--tag'] = 'views';
+                    break;
+
+                case 'lang':
+                    $this->info('Publish just a part: lang.');
+                    $callArguments['--tag'] = 'lang';
+                    break;
+
+                default:
+                    $this->error('Invalid "only" argument value!');
+                    return;
+                    break;
+            }
+
+        } else {
+            $this->info('Publish all parts: views, lang.');
+        }
+
+        if ($this->option('force')) {
+            $this->warn('Force publishing.');
+            $callArguments['--force'] = true;
+        }
+
+        $this->call('vendor:publish', $callArguments);
+    }
+}
--- src/GridViewServiceProvider.php
+++ src/GridViewServiceProvider.php
@@ -4,6 +4,7 @@ namespace Itstructure\GridView;
 
 use Illuminate\Support\Facades\Blade;
 use Illuminate\Support\ServiceProvider;
+use Itstructure\GridView\Commands\PublishCommand;
 
 /**
  * Class GridViewServiceProvider
@@ -11,17 +12,105 @@ use Illuminate\Support\ServiceProvider;
  */
 class GridViewServiceProvider extends ServiceProvider
 {
+    public function register()
+    {
+        $this->registerCommands();
+    }
+
     public function boot()
     {
-        $this->loadViewsFrom($this->packagePath('resources/views'), 'grid_view');
-        $this->loadTranslationsFrom($this->packagePath('resources/lang'), 'grid_view');
+        // Loading settings
+        $this->loadViews();
+        $this->loadTranslations();
 
+        // Publish settings
+        $this->publishViews();
+        $this->publishTranslations();
+
+        // Directives
         require_once __DIR__ . '/functions.php';
 
         Blade::directive('gridView', function ($config) {
             return "<?php echo grid_view($config); ?>";
         });
     }
+
+
+    /*
+    |--------------------------------------------------------------------------
+    | COMMAND SETTINGS
+    |--------------------------------------------------------------------------
+    */
+
+    /**
+     * Register commands.
+     * @return void
+     */
+    private function registerCommands(): void
+    {
+        $this->commands(Commands\PublishCommand::class);
+    }
+
+
+    /*
+    |--------------------------------------------------------------------------
+    | LOADING SETTINGS
+    |--------------------------------------------------------------------------
+    */
+
+    /**
+     * Set directory to load views.
+     * @return void
+     */
+    private function loadViews(): void
+    {
+        $this->loadViewsFrom($this->packagePath('resources/views'), 'grid_view');
+    }
+
+    /**
+     * Set directory to load translations.
+     * @return void
+     */
+    private function loadTranslations(): void
+    {
+        $this->loadTranslationsFrom($this->packagePath('resources/lang'), 'grid_view');
+    }
+
+
+    /*
+    |--------------------------------------------------------------------------
+    | PUBLISH SETTINGS
+    |--------------------------------------------------------------------------
+    */
+
+    /**
+     * Publish views.
+     * @return void
+     */
+    private function publishViews(): void
+    {
+        $this->publishes([
+            $this->packagePath('resources/views') => resource_path('views/vendor/grid_view'),
+        ], 'views');
+    }
+
+    /**
+     * Publish translations.
+     * @return void
+     */
+    private function publishTranslations(): void
+    {
+        $this->publishes([
+            $this->packagePath('resources/lang') => resource_path('lang/vendor/grid_view'),
+        ], 'lang');
+    }
+
+
+    /*
+    |--------------------------------------------------------------------------
+    | OTHER SETTINGS
+    |--------------------------------------------------------------------------
+    */
 
     /**
      * @param $path
